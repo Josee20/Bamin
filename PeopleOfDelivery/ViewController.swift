89f3a3ed4877be5da348bf406e22a1085178edf7
LDK <dlehdrl115@naver.com>
2022-07-08 09:42:15 +0900

    깃헙 커밋 테스트

--- PeopleOfDelivery/ViewController.swift
+++ PeopleOfDelivery/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print(1)
+        print(2)
+        print(3)
+        print(4)
     }
 
 
