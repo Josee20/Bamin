c18df5c1f3b08e9b08ae5ba37fa8769828876ffe
LDK <dlehdrl115@naver.com>
2022-07-12 09:04:31 +0900

    카카오톡 오토레이아웃 완료

---
+++ PeopleOfDelivery/BaminHomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  KakaoTalkViewController.swift
+//  PeopleOfDelivery
+//
+//  Created by 이동기 on 2022/07/11.
+//
+
+import UIKit
+
+class BaminHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
